ba7482ac276d7dbee40b4a2ccc147c6f3fea5111
vivekbharatha <vivek.bharatha@gmail.com>
2018-01-06 00:57:47 +0530

    anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,43 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    return cleanString(stringA) === cleanString(stringB);
+}
+
+function cleanString(str) {
+    return str.replace(/[^\w]/g,'').toLowerCase().split('').sort().join('');
+}
 
 module.exports = anagrams;
+
+
+// Solution 1
+/* 
+function anagrams(stringA, stringB) {
+    const AMap = buildCharMap(stringA);
+    const BMap = buildCharMap(stringB);
+
+    if (Object.keys(AMap).length !== Object.keys(BMap).length) {
+        return false;
+    }
+
+    for (let char in AMap) {
+        if (AMap[char] !== BMap[char]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function buildCharMap(str) {
+    const charMap = {};
+
+    for(let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+        charMap[char] = charMap[char] + 1 || 1;
+    }
+
+    return charMap;
+};
+*/
