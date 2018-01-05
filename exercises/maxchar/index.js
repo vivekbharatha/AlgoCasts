8f8849ce0f2b782cef962a09e86f75e5d4258b37
vivekbharatha <vivek.bharatha@gmail.com>
2018-01-06 00:06:21 +0530

    maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,26 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const charMap = {};
+    for(let char of str) {
+        if (!charMap[char]) {
+            charMap[char] = 1;
+        } else {
+            charMap[char]++;
+        }
+    }
+
+    let max = 0;
+    let maxChar = '';
+
+    for(let char in charMap) {
+        if (charMap[char] > max) {
+            max = charMap[char];
+            maxChar = char;
+        }
+    }
+    return maxChar;
+}
 
 module.exports = maxChar;
